fa41712abf5a90ef5d620ae855bfda86b76f1882
Sam Zhang <nanzhang@yufuid.com>
2020-12-22 21:17:00 +0800

    [week1] 修复无创建目录问题,增加循环调用函数

--- week01/homework/main.py
+++ week01/homework/main.py
@@ -5,6 +5,7 @@ import logging
 import time
 import requests
 from pathlib import Path
+import sleep
 
 '''
 编写一个函数, 当函数被调用时，将调用的时间记录在日志中, 
@@ -24,18 +25,22 @@ def get_host_ip_add():
     logging.info(f"host ip add is {ip_add}")
     return ip_add
 
-# 生成logging配置
-def create_logging_config(log_dir_date_value):
+# 获取log目录
+def get_logs(log_dir_date_value):
     create_logging_dir(log_dir_date_value)
-    log_dir = f"/var/log/python-{log_dir_date_value}"
+    log_dir = f"/Users/Sam/samtest/python-{log_dir_date_value}"
     logs = f"{log_dir}/sam-test.log"
+    return logs
 
+# 生成logging配置
+def create_logging_config(logs):
+    create_logging_dir(logs)
     logging.basicConfig(filename=logs,
-                        encoding='utf-8', level=logging.INFO)
+                        level=logging.INFO)
 
 # 创建日志目录
-def create_logging_dir(log_dir_date_value):
-    p = Path(log_dir_date_value)
+def create_logging_dir(logs):
+    p = Path(logs)
     if not p.parent.exists() :
         try:
             # todo: 获取root权限
@@ -46,8 +51,12 @@ def create_logging_dir(log_dir_date_value):
 # 主函数
 def main():
     log_dir_date_value=time.strftime(TIME_FORMAT)
-    create_logging_config(log_dir_date_value)
-    get_host_ip_add()
+    logs = get_logs(log_dir_date_value)
+    while True:
+        create_logging_config(logs)
+        get_host_ip_add()
+        time.sleep(5)
+        
 
 if __name__ == "__main__":
     main()
